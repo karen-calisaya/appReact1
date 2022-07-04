6982fb229d8adb84f10c1bce34ef7756fe050651
Karen Calisaya <karenmcalisaya@gmail.com>
2022-07-04 15:41:06 -0300

    topbarComponent

---
+++ karen-app/src/components/App.js
@@ -0,0 +1,14 @@
+import '../assets/css/app.css';
+import TopBar from './TopBar';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>hola</h1>
+       <TopBar />
+
+    </div>
+  );
+}
+
+export default App;
