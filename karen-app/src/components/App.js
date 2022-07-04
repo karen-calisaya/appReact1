bf6fe167e933e56c01fd881dbbdb008ec9f89e0a
Karen Calisaya <karenmcalisaya@gmail.com>
2022-07-04 15:56:18 -0300

    sideBar component

--- karen-app/src/components/App.js
+++ karen-app/src/components/App.js
@@ -1,11 +1,13 @@
 import '../assets/css/app.css';
 import TopBar from './TopBar';
+import SideBar from './SideBar';
 
 function App() {
   return (
     <div className="App">
        <h1>hola</h1>
        <TopBar />
+       <SideBar />
 
     </div>
   );
